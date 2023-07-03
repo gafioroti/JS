145e0b07940b1a48a6848c993c9dcefb608b2125
Gabriel Fioroti <gabriel.fioroti@hotmail.com>
2023-07-03 16:14:48 -0300

    Create ex008.js

---
+++ aula05/ex008.js
@@ -0,0 +1,7 @@
+var vel = 30
+console.log(`A velociade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite permitido. MULTADO!')
+} else {
+    console.log('Você está dentro do limite de velocidade.')
+}
