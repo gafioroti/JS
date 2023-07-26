d8bcbfd2db9ef4ff8b90b30c15b82f62ae2b0f0d
Gabriel Fioroti <gabriel.fioroti@hotmail.com>
2023-07-26 14:45:10 -0300

    Update script.js

--- aula10/ex015/script.js
+++ aula10/ex015/script.js
@@ -8,10 +8,14 @@ function contar() {
         window.alert('[ERRO] Faltam dados!')
         res.innerHTML = 'Impossível contar'
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>'
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passos.value)
+            if( p <= 0) {
+                window.alert('Passo Inválido! min = 1')
+                p = 1
+            }
             if( i < f ) {
                 for(var c = i; c <= f; c += p) {
                     res.innerHTML += `${c} \u{1F449}`
